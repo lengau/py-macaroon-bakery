4efe7332f1a9c3070fa56168e92a17a7272acecc
Alex Lowe <alex.lowe@canonical.com>
2024-09-18 20:51:12 -0400

    Add brew installation to Makefile

--- sysdeps.mk
+++ sysdeps.mk
@@ -1,1 +1,2 @@
-APT_SYSDEPS = python3-pip python3-setuptools libsodium-dev libffi-dev libssl-dev
+APT_SYSDEPS = python3-pip python3-setuptools libsodium-dev libffi-dev libssl-dev tox isort
+BREW_SYSDEPS = python3 libsodium libffi openssl tox isort
